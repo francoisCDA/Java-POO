b4c5361c1bbb24e68ab5e31c90bcc97bf404e793
François Termote <francois.termote@gmail.com>
2023-12-07 13:58:01 +0100

    navigation

--- DesignPattern/src/main/java/org/Exercices/Strategie/OffRoadStrategy.java
+++ DesignPattern/src/main/java/org/Exercices/Strategie/OffRoadStrategy.java
@@ -3,6 +3,6 @@ package org.Exercices.Strategie;
 public class OffRoadStrategy implements NavigationStrategy{
     @Override
     public String navigate(String destination) {
-        return "\nCalcule d'itiniéraire ..... \n\t ->Traverse le fleuve à la nage, c'est juste après le champ";
+        return "\nCalcule d'itiniérairs + " + destination + " ..... \n\t ->Traverse le fleuve à la nage, c'est juste après le champ";
     }
 }
