12323a1b57237957db03f70085feaf36431ef818
François Termote <francois.termote@gmail.com>
2023-12-07 13:59:42 +0100

    navigation

--- DesignPattern/src/main/java/org/Exercices/Strategie/EconomicStrategy.java
+++ DesignPattern/src/main/java/org/Exercices/Strategie/EconomicStrategy.java
@@ -3,6 +3,6 @@ package org.Exercices.Strategie;
 public class EconomicStrategy implements NavigationStrategy {
     @Override
     public String navigate(String destination) {
-        return "\nCalcule d'itiniérairs " + destination + " ..... \n\t ->Lève le pouce au bord de la route, et oublis pas de sourir";
+        return "\nCalcule d'itiniérairs " + destination + " ..... \n\t -> Lève le pouce au bord de la route, et oublis pas de sourir";
     }
 }
